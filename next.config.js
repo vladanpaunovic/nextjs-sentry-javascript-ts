007a86f03e9776b0ab9667ea20324d2bc181deb5
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2022-05-04 10:54:25 +0200

    chore: init Sentry

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+const withPlugins = require("next-compose-plugins");
+const withOffline = require("next-offline");
+const { withSentryConfig } = require("@sentry/nextjs");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const sentryWebpackPluginOptions = {
+  silent: true,
+};
+
+const allPlugins = withPlugins([withOffline], nextConfig);
+
+module.exports = withSentryConfig(allPlugins, sentryWebpackPluginOptions);

aa7146bedd653384ef4418ef7250defede61644e
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2022-05-30 11:24:32 +0200

    chore: as

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -4,7 +4,9 @@ export async function middleware(req: NextRequest, res: NextResponse) {
   // Store the response so we can modify its headers
   const response = NextResponse.next();
 
-  throw new Error("Error from Middleware!");
+  if (req.url.includes("hello")) {
+    throw new Error("Error from Middleware!");
+  }
 
   // Set custom header
   response.headers.set("x-hello-from", "middleware");
--- pages/api/throw.ts
+++ pages/api/throw.ts
@@ -10,6 +10,8 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  throw new Error("Error from API throw.js");
+
   res.status(200).json({
     name: "John Doe",
     res_headers: res.getHeaders(),
